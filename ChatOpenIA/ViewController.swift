efc71834cf008dfde654f76a7be61ec18cb0e7a9
Rafael Oliveira <oliveirafael@icloud.com>
2023-06-04 20:50:50 -0300

    Initial Commit

---
+++ ChatOpenIA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatOpenIA
+//
+//  Created by Rafael Oliveira on 04/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
