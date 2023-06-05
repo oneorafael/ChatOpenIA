d774c9a329b2291598f68d5be403e9ed562c99c9
Rafael Oliveira <oliveirafael@icloud.com>
2023-06-05 10:57:35 -0300

    - Fix scrollView bug - Add UIActivityIndicatorView when API is loading.

--- ChatOpenIA/View/ViewController.swift
+++ ChatOpenIA/View/ViewController.swift
@@ -16,15 +16,20 @@ class ViewController: UIViewController {
         responseLabel.textColor = .secondaryLabel
         return responseLabel
     }()
+    let progressIndicator = UIActivityIndicatorView()
+    
     private let scrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.isScrollEnabled = true
+        scrollView.showsHorizontalScrollIndicator = false
         return scrollView
     }()
     private let inputTextField: UITextField = {
         let inputTextField = UITextField()
         inputTextField.translatesAutoresizingMaskIntoConstraints = false
         inputTextField.placeholder = "Faça a sua pergunta"
+        inputTextField.text = "Quem é Selena gomez"
         inputTextField.returnKeyType = .done
         return inputTextField
     }()
@@ -37,9 +42,12 @@ class ViewController: UIViewController {
         setupConstraints()
         responseLabel.text = ""
         inputTextField.delegate = self
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: responseLabel.frame.size.height)
+        progressIndicator.translatesAutoresizingMaskIntoConstraints = false
     }
     
     private func setup(){
+        view.addSubview(progressIndicator)
         view.addSubview(scrollView)
         scrollView.addSubview(responseLabel)
         view.addSubview(inputTextField)
@@ -49,15 +57,18 @@ class ViewController: UIViewController {
         let safeArea = view.safeAreaLayoutGuide
         NSLayoutConstraint.activate([
             scrollView.topAnchor.constraint(equalTo: safeArea.topAnchor),
-            scrollView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 10),
-            scrollView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -10),
-            scrollView.bottomAnchor.constraint(equalTo:inputTextField.topAnchor, constant: -5),
+            scrollView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: inputTextField.topAnchor),
             
-            responseLabel.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            responseLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 16),
             responseLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
             responseLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
-            responseLabel.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-            
+            responseLabel.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -16),
+            responseLabel.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -32),
+                    
+            progressIndicator.centerYAnchor.constraint(equalTo: safeArea.centerYAnchor),
+            progressIndicator.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
             inputTextField.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
             inputTextField.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
             inputTextField.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor, constant: -20),
@@ -70,14 +81,20 @@ class ViewController: UIViewController {
 
 extension ViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        progressIndicator.isHidden = false
+        progressIndicator.startAnimating()
         responseLabel.text = ""
+        self.inputTextField.resignFirstResponder()
         if let text = textField.text, !text.isEmpty {
             connection.getResponse(input: text) { response in
                 switch response {
+                    
                 case .success(let responseText):
                     DispatchQueue.main.async {
                         self.responseLabel.text = responseText
                         self.inputTextField.text = ""
+                        self.progressIndicator.stopAnimating()
+                        self.progressIndicator.isHidden = true
                     }
                 case .failure:
                     print("Erro ao buscar na API")
